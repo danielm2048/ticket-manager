452fae9013f9b5877cc4911799f6a4094ebcca94
danielm2048 <danielmimoun98@gmail.com>
2021-03-24 21:58:16 +0200

    changed done and creationTime params to default

--- models/tickets.model.js
+++ models/tickets.model.js
@@ -6,8 +6,8 @@ const ticketSchema = new Schema({
 	title: { type: String, required: true },
 	content: { type: String, required: true },
 	userEmail: { type: String, required: true },
-	done: { type: Boolean, required: true },
-	creationTime: { type: Date, required: true },
+	done: { type: Boolean, default: false },
+	creationTime: { type: Date, default: new Date() },
 	labels: [{ type: Date, required: true }],
 });
 
